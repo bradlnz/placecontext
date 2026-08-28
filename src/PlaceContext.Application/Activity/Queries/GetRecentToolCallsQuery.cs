c16ee1d86c9a72b5388416581bda48d65110a00c
Brad Lietz <brad.lietz@outlook.com>
2026-08-28 17:02:33 +1000

    Remove unused recent tool call imports

--- src/PlaceContext.Application/Activity/Queries/GetRecentToolCallsQuery.cs
+++ src/PlaceContext.Application/Activity/Queries/GetRecentToolCallsQuery.cs
@@ -1,13 +1,5 @@
 using PlaceContext.Application.Cqrs;
 using PlaceContext.Application.Dtos;
-using PlaceContext.Application.Ports;
-using PlaceContext.Domain.Entities;
-using PlaceContext.Domain.Repositories;
-using PlaceContext.Domain.Services;
-using PlaceContext.Domain.ValueObjects;
 
 namespace PlaceContext.Application.Features;
-
-// ---- Recent MCP tool calls (Inspector) ----
-
 public sealed record GetRecentToolCallsQuery(int Take = 100) : IQuery<IReadOnlyList<ToolCallView>>;
